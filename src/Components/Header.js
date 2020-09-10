5ca23b6dd0d1890a9cbfbf4000425078ba5ab40d
Michelle Rogers <michelleevarogers@gmail.com>
2020-09-09 20:55:24 -0700

    Tweaks to manifest.json and header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faHamburger } from '@fortawesome/free-solid-svg-icons'
+
 const Header = ({ title }) => {
   return (
     <header>
+      <span className="burger">
+        <FontAwesomeIcon icon={faHamburger} />
+      </span>
       { title }
     </header>
   )
